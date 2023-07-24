fbdf99aa7ab3f5f1ec774877fe158b29f7c84d18
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-24 12:55:57 +0100

    useMemo

--- src/components/Hooks.tsx
+++ src/components/Hooks.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, ChangeEvent } from "react";
+import { useState, useEffect, useCallback, useMemo, ChangeEvent } from "react";
 
 interface User {
   id: number;
@@ -12,20 +12,22 @@ const Hooks = () => {
 
   const [input, setInput] = useState<string>("");
 
+  const [myNumber, setMyNumber] = useState<number>(30);
+
   // useEffect deals with Side Effects, so it doesn't really return a value (same as useLayoutEffect)
   // so with TypeScript there is nothing much to do
   // Side Effects are "when something changes, something else can happen"
   // it doesn't return a value but maybe you want to change something else
 
   useEffect(() => {
-    console.log("Mounting...");
+    console.log("useEffect Mounting...");
     // When you use Strict Mode it mounts the Component
     // then it unmounts it, then it remounts it again
 
-    console.log("Users: ", users);
+    console.log("useEffect Users: ", users);
 
     // useEffect does return a Clean Up function
-    return () => console.log("Unmounting...");
+    return () => console.log("useEffect Unmounting...");
   }, [users]);
 
   const handleAddUser = () => {
@@ -62,12 +64,27 @@ const Hooks = () => {
     []
   );
 
+  // useMemo memoizes a value, something that takes a while to calculate and might hold up the application
+  type fibonacciFunctionType = (n: number) => number;
+
+  const fibonacciFunction: fibonacciFunctionType = (n) => {
+    if (n < 2) return n;
+    return fibonacciFunction(n - 1) + fibonacciFunction(n - 2);
+  };
+
+  // const myNumber: number = 30;
+
+  const result = useMemo(() => {
+    console.log("useMemo of fibonacciFunction");
+    return fibonacciFunction(myNumber);
+  }, [myNumber]);
+
   return (
     <div>
       <hr />
       <h2>Hooks</h2>
       <hr />
-      <h3>useState</h3>
+      <h3>useState & useCallback</h3>
       <p>Count: {count}</p>
       <div>
         <button onClick={decrementCount}>-</button>
@@ -87,6 +104,16 @@ const Hooks = () => {
         <input type="text" value={input} onChange={handleInputChange} />
         <button onClick={handleAddUser}>Add User</button>
       </div>
+      <hr />
+      <h3>useMemo</h3>
+      <input
+        type="number"
+        value={myNumber}
+        min={0}
+        max={37}
+        onChange={(event) => setMyNumber(parseInt(event.target.value))}
+      />
+      <p>Fibonacci Number: {result}</p>
       <hr />
     </div>
   );
