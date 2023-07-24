a895cb16dbaf61672b45c310428512d734324309
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-24 09:46:34 +0100

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Heading from "./components/Heading";
 import Section from "./components/Section";
+import Counter from "./components/Counter";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
       <Heading title={"Hello"} />
       {/* children are not the same as props, they are put between the opening/closing tags */}
       <Section title={"A different title"}>This is my Section</Section>
+      <Counter></Counter>
     </>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const Counter = () => {
+  // Inferred Type:
+  // const [count, setCount] = useState(1);
+  // Explicit Type:
+  const [count, setCount] = useState<number>(1);
+  // Union Type:
+  // const [count, setCount] = useState<number | null>(null);
+
+  return (
+    <>
+      <h1>Count is {count}</h1>
+      <div>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          +
+        </button>
+        <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+          -
+        </button>
+      </div>
+    </>
+  );
+};
+export default Counter;
